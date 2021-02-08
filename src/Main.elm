932d1bd91637f20cc520ca6f8a065d66c76c358d
Laurens <laurens.decock@odisee.be>
2021-02-08 15:20:07 +0100

    ♻️ model.itemSpawned is now in model.gameState; itemSpawned is now Spawn 💄 External font loader, UI code splitting, added styles, small animations

--- src/Main.elm
+++ src/Main.elm
@@ -2,9 +2,12 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Element as Ui
-import Element.Input
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
 import Html
-import Html.Attributes exposing (..)
+import Html.Attributes
 import Html.Events exposing (..)
 import Json.Decode
 import Random
@@ -28,15 +31,17 @@ main =
 
 
 type alias Model =
-    { itemSpawned : ItemSpawned
+    { itemSpawned : Spawn
     , inputGuess : String
     , theRightAnswer : Int
+    , gameState : GameState
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( { itemSpawned = { time = 0, item = Red }
+      , gameState = Loading
       , inputGuess = ""
       , theRightAnswer = 0
       }
@@ -50,12 +55,18 @@ init _ =
 
 type Msg
     = GeneratingItem
-    | GeneratedItem ItemSpawned
+    | GeneratedItem Spawn
     | InputChanged String
     | GuessSubmitted
 
 
-type alias ItemSpawned =
+type GameState
+    = Loading
+    | Guessed String
+    | ItemSpawned Spawn
+
+
+type alias Spawn =
     { time : Int, item : Item }
 
 
@@ -68,23 +79,28 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GeneratingItem ->
-            ( model, Random.generate GeneratedItem itemSpawnedGenerator )
+            ( model, generateNewSpawn )
 
         GeneratedItem itemSpawned ->
-            ( { model | itemSpawned = itemSpawned }, Cmd.none )
+            ( { model | gameState = ItemSpawned itemSpawned }, Cmd.none )
 
         InputChanged txt ->
             ( { model | inputGuess = txt }, Cmd.none )
 
         GuessSubmitted ->
-            validateAnswer model
+            ( validateAnswer model, generateNewSpawn )
+
+
+generateNewSpawn : Cmd Msg
+generateNewSpawn =
+    Random.generate GeneratedItem itemSpawnedGenerator
 
 
 
 -- Generate spawn time for Red or Mega
 
 
-validateAnswer : Model -> ( Model, Cmd Msg )
+validateAnswer : Model -> Model
 validateAnswer model =
     let
         item =
@@ -112,12 +128,12 @@ validateAnswer model =
                     else
                         time + 35
     in
-    ( { model | theRightAnswer = theRightAnswer }, Cmd.none )
+    { model | theRightAnswer = theRightAnswer }
 
 
-itemSpawnedGenerator : Random.Generator ItemSpawned
+itemSpawnedGenerator : Random.Generator Spawn
 itemSpawnedGenerator =
-    Random.map2 ItemSpawned timeGenerator itemGenerator
+    Random.map2 Spawn timeGenerator itemGenerator
 
 
 timeGenerator : Random.Generator Int
@@ -145,21 +161,83 @@ subscriptions model =
 
 view : Model -> Html.Html Msg
 view model =
-    Ui.layout [] <|
-        Ui.column
-            [ Ui.centerX, Ui.centerY, Ui.paddingEach { top = 0, right = 0, bottom = 200, left = 0 } ]
-            [ Ui.text (itemToString model.itemSpawned.item ++ " spawned at " ++ String.fromInt model.itemSpawned.time)
-            , Element.Input.text
-                [ onEnter GuessSubmitted ]
+    Ui.layoutWith
+        { options =
+            -- Global focus style
+            [ Ui.focusStyle
+                { borderColor = Just (Ui.rgb 0.2 0.2 0.8)
+                , backgroundColor = Nothing
+                , shadow = Nothing
+                }
+            ]
+        }
+        []
+        (Ui.column
+            -- Main UI component
+            [ Ui.centerX
+            , Ui.centerY
+            , Ui.paddingEach { top = 0, right = 0, bottom = 200, left = 0 }
+            , Ui.width Ui.shrink
+
+            -- Global font styles
+            , Font.family
+                [ Font.external { name = "Open Sans", url = "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&display=swap" }
+                , Font.typeface "Verdana"
+                , Font.sansSerif
+                ]
+            , Font.size 18
+            , Font.color (color Text)
+            , Ui.spacing 16
+            ]
+            (viewCoreGame model)
+        )
+
+
+viewCoreGame : Model -> List (Ui.Element Msg)
+viewCoreGame model =
+    case model.gameState of
+        Loading ->
+            [ Ui.none ]
+
+        ItemSpawned spawn ->
+            let
+                spawncolor =
+                    if spawn.item == Red then
+                        Ui.rgb 1 0 0
+
+                    else
+                        Ui.rgb 0 1 1
+            in
+            [ Ui.paragraph []
+                -- Red armor / mega spawned at
+                [ Ui.el [ Font.bold, Font.color spawncolor ] (Ui.text (itemToString spawn.item))
+                , Ui.text " spawned at "
+                , Ui.el [ Font.bold ] (Ui.text (String.fromInt spawn.time))
+                ]
+            , Input.text
+                [ onEnter GuessSubmitted
+                , Ui.width (Ui.shrink |> Ui.minimum 80)
+                , Input.focusedOnLoad
+                ]
                 { onChange = InputChanged
                 , text = model.inputGuess
-                , placeholder = Just (Element.Input.placeholder [] (Ui.text "placeholder"))
-                , label = Element.Input.labelLeft [] (Ui.text "Next item at xx:")
+                , placeholder = Just (Input.placeholder [] (Ui.text "25"))
+                , label = Input.labelLeft [ Font.semiBold ] (Ui.text "Next item at xx:")
                 }
-            , Element.Input.button
-                []
-                { onPress = Just GuessSubmitted, label = Ui.text "Guess" }
+            , Input.button
+                [ Border.width 1
+                , Border.rounded 8
+                , Ui.paddingXY 32 8
+                , Ui.mouseOver [ Font.color <| Ui.rgb 1 1 1, Background.color <| Ui.rgb 0.2 0.2 0.2 ]
+                , smoothTransition
+                ]
+                { onPress = Just GuessSubmitted
+                , label = Ui.text "Guess"
+                }
             ]
+
+        Guessed x ->
+            [ Ui.none ]
 
 
 itemToString : Item -> String
@@ -187,3 +265,20 @@ onEnter msg =
                     )
             )
         )
+
+
+smoothTransition : Ui.Attribute msg
+smoothTransition =
+    Ui.htmlAttribute (Html.Attributes.style "transition" "0.15s ease-in-out")
+
+
+type AppColor
+    = Text
+
+
+color : AppColor -> Ui.Color
+color col =
+    case col of
+        -- Ui.rgb255 0x33 0x33 0x33
+        Text ->
+            Ui.rgb 0.2 0.2 0.2
